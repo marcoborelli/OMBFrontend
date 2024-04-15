5d43fcfdbaeb299b7adc8a31504576e8b4b00e49
Borelli Marco <Borelli.19312>
2024-04-15 10:25:07 +0200

    \src\components\CardTest.jsx: aggiunto riferimento all'elemento GraphTest

--- src/components/CardTest.jsx
+++ src/components/CardTest.jsx
@@ -1,23 +1,15 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import { LineChart } from '@mui/x-charts/LineChart';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import GraphTest from './GraphTest'
 
 export default function ActionAreaCard({ valve_id, timestamp, arr_data }) {
     return (
         <Card sx={{ maxWidth: 345 }} style={{ backgroundColor: '#A2C6D8' }}>
             <CardActionArea>
-                <LineChart
-                    xAxis={[{ data: arr_data.map(val => val.angle) }]} //pair
-                    series={[{
-                        data: arr_data.map(val => val.pair),
-                        area: true,
-                        showMark: false
-                    }]} //angle
-                    height={230}
-                />
+                <GraphTest height="230" data={arr_data} />
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
                         {valve_id}
