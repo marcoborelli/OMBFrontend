caa060e939ee2540070a69ad12c0a887a36b9ae7
Borelli Marco <Borelli.19312>
2024-04-15 10:24:18 +0200

    \src\components\GraphTest.jsx: creato componente che rappresenta il grafico di una misurazione

---
+++ src/components/GraphTest.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { LineChart } from '@mui/x-charts/LineChart';
+
+export default function GraphTest({ height, data }) {
+    return (
+        <LineChart
+            xAxis={[{ data: data.map(val => val.angle) }]} //pair
+            series={[{
+                data: data.map(val => val.pair),
+                area: true,
+                showMark: false
+            }]} //angle
+            height={height}
+        />
+    );
+}
