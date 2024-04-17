615d79b5a9582986dd63cc608475dc0fd02702c9
marcoborelli <marco05.borelli@gmail.com>
2024-04-17 17:39:59 +0200

    \src\components\CardValveInstance.jsx: aggiunta card che rappresenta l'istanza di valvola

---
+++ src/components/CardValveInstance.jsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { CardActionArea, Typography, CardContent, Card } from '@mui/material'
+
+export default function CardValveInstance({ serial_number, job_number }) {
+    return (
+        <Card sx={{ width: 300 }} style={{ backgroundColor: '#FFECCB' }}>
+            <CardActionArea>
+                <CardContent>
+                    <Typography gutterBottom variant="h5" align="center" component="div">
+                        {serial_number}
+                    </Typography>
+                    <Typography variant="body2" align="center" color="text.secondary">
+                        {job_number}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    );
+}
