8668d4a1d11be04db96f8ebb6af5a08d39f7129e
marcoborelli <marco05.borelli@gmail.com>
2024-04-20 15:10:35 +0200

    \src\pages\PageGenericTest.jsx: la pagina fa ora chiamate al backend   Aggiunto collegamento alla pagina del test specifico alle card

--- src/pages/PageGenericTest.jsx
+++ src/pages/PageGenericTest.jsx
@@ -1,17 +1,44 @@
-import { useState, useEffect } from 'react';
-import { Grid } from '@mui/material';
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Grid } from '@mui/material'
 import CardTest from '../components/CardTest'
+import Loading from '../components/Loading'
+import api from '../services/api'
 
-export default function PageGenericTest({ data }) {
+export default function PageGenericTest() {
+    const [tests, setTests] = useState()
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await api.get(`api/tests/all`)
+                setTests(response.data)
+            } catch (error) {
+                console.error('Error fetching user data:', error)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchData()
+    }, [])
+
+    if (loading) {
+        return <Loading text="Caricamento in corso..."></Loading>
+    }
+
     return (
         <Grid item style={{ maxHeight: '100vh', overflowY: 'auto' }}>
             <Grid container spacing={2} justifyContent="center">
-                {data.map((d) => (
-                    <Grid item>
-                        <CardTest valve_id={d.valve_id} timestamp={d.timestamp} arr_data={d.data} key={`${d._id}`} />
+                {tests.map((d) => (
+                    <Grid item key={d._id}>
+                        <Link to={`/tests/${d._id}`} style={{ textDecoration: 'none' }}>
+                            <CardTest valve_id={d.valve_id} timestamp={d.timestamp} arr_data={d.data} />
+                        </Link>
                     </Grid>
                 ))}
             </Grid>
-        </Grid>
-    );
-};
+        </Grid >
+    )
+}
