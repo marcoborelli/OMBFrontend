55c0e7657681a30bb95780d21d7370eb68fc7686
marcoborelli <marco05.borelli@gmail.com>
2024-04-20 15:13:45 +0200

    \src\pages\PageGenericValveModel.jsx: aggiunta chiamata ad api del backend   Aggiunto collegamento alla pagina dell'istanza specifica alle card

--- src/pages/PageGenericValveModel.jsx
+++ src/pages/PageGenericValveModel.jsx
@@ -1,17 +1,44 @@
-import { useState, useEffect } from 'react';
-import { Grid } from '@mui/material';
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Grid } from '@mui/material'
 import CardValveModel from '../components/CardValveModel'
+import Loading from '../components/Loading'
+import api from '../services/api'
 
-export default function PageGenericValveModel({ valves }) {
+export default function PageGenericValveModel() {
+    const [valves, setValves] = useState()
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await api.get(`api/valves/all`)
+                setValves(response.data)
+            } catch (error) {
+                console.error('Error fetching user data:', error)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchData()
+    }, [])
+
+    if (loading) {
+        return <Loading text="Caricamento in corso..."></Loading>
+    }
+
     return (
         <Grid item style={{ maxHeight: '100vh', overflowY: 'auto' }}>
             <Grid container spacing={2} justifyContent="center">
                 {valves.map((valve) => (
-                    <Grid item>
-                        <CardValveModel code={valve._id} description={valve.description} img_url={valve.img_url} key={`${valve._id}`} />
+                    <Grid item key={valve._id}>
+                        <Link to={`/models/${valve._id}`} style={{ textDecoration: 'none' }}>
+                            <CardValveModel code={valve._id} description={valve.description} img_url={valve.img_url} />
+                        </Link>
                     </Grid>
                 ))}
             </Grid>
         </Grid>
-    );
-};
+    )
+}
