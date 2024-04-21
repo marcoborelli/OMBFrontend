379d533238bebfc8833bb531c4295348c242dc45
marcoborelli <marco05.borelli@gmail.com>
2024-04-21 16:13:49 +0200

    \src\components\Navbar.jsx: implementata Navbar all'interno della pagina

--- src/pages/PageGenericTest.jsx
+++ src/pages/PageGenericTest.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { Grid } from '@mui/material'
 import CardTest from '../components/CardTest'
 import Loading from '../components/Loading'
+import Navbar from '../components/Navbar'
 import api from '../services/api'
 
 export default function PageGenericTest() {
@@ -29,16 +30,19 @@ export default function PageGenericTest() {
     }
 
     return (
-        <Grid item style={{ maxHeight: '100vh', overflowY: 'auto', paddingTop: '3vh' }}>
-            <Grid container spacing={2} justifyContent="center">
-                {tests.map((d) => (
-                    <Grid item key={d._id}>
-                        <Link to={`/tests/${d._id}`} style={{ textDecoration: 'none' }}>
-                            <CardTest instance_id={d.instance_id} timestamp={d.timestamp} arr_data={d.data} />
-                        </Link>
-                    </Grid>
-                ))}
-            </Grid>
-        </Grid >
+        <>
+            <Navbar />
+            <Grid item style={{ maxHeight: '100vh', overflowY: 'auto', paddingTop: '3vh' }}>
+                <Grid container spacing={2} justifyContent="center">
+                    {tests.map((d) => (
+                        <Grid item key={d._id}>
+                            <Link to={`/tests/${d._id}`} style={{ textDecoration: 'none' }}>
+                                <CardTest instance_id={d.instance_id} timestamp={d.timestamp} arr_data={d.data} />
+                            </Link>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Grid >
+        </>
     )
 }
