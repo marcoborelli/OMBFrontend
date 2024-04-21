f43ce8db24363b63bda72218d401e7ab55c5e141
marcoborelli <marco05.borelli@gmail.com>
2024-04-21 15:14:57 +0200

    \src\services\utilities.js: aggiunta funzione che passato il codice di errore restituisce la pagina relativa

--- src/services/utilities.js
+++ src/services/utilities.js
@@ -1,3 +1,5 @@
+import PageError404 from '../pages/PageError404'
+
 export function getLastOpeningIndex(data) {
     let i = 0
     let lastAngle = -1
@@ -11,4 +13,14 @@ export function getLastOpeningIndex(data) {
     }
 
     return i
+}
+
+export function getErrorPage(error) {
+    switch (error) {
+        case 404:
+            return PageError404()
+        case 500:
+            //TODO
+            break
+    }
 }
