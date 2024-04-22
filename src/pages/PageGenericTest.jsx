b6d4b78181540cfd505a4af5ae5cbc5876274499
marcoborelli <marco05.borelli@gmail.com>
2024-04-22 21:23:32 +0200

    \src\pages\PageGenericTest.jsx: aggiunta barra di ricerca alla pagina   riviste le proporzioni degli altri componenti

--- src/pages/PageGenericTest.jsx
+++ src/pages/PageGenericTest.jsx
@@ -4,10 +4,12 @@ import { Grid } from '@mui/material'
 import CardTest from '../components/CardTest'
 import Loading from '../components/Loading'
 import Navbar from '../components/Navbar'
+import Searchbar from '../components/Searchbar'
 import api from '../services/api'
 
 export default function PageGenericTest() {
     const [tests, setTests] = useState()
+    const [filteredTests, setFilteredTests] = useState()
     const [loading, setLoading] = useState(true)
 
     useEffect(() => {
@@ -15,6 +17,7 @@ export default function PageGenericTest() {
             try {
                 const response = await api.get(`api/tests/all`)
                 setTests(response.data)
+                setFilteredTests(response.data)
             } catch (error) {
                 console.error('Error fetching user data:', error)
             } finally {
@@ -25,6 +28,12 @@ export default function PageGenericTest() {
         fetchData()
     }, [])
 
+    function filterTests(e) {
+        setFilteredTests(tests.filter((test) => {
+            return test.instance_id.toLowerCase().trim().includes(e.target.value.toLowerCase().trim())
+        }))
+    }
+
     if (loading) {
         return <Loading text="Caricamento in corso..."></Loading>
     }
@@ -32,9 +41,12 @@ export default function PageGenericTest() {
     return (
         <>
             <Navbar />
-            <Grid item style={{ paddingTop: '3vh' }}>
+            <Grid item style={{ paddingTop: '3vh', paddingBottom: '3vh', display: 'flex', justifyContent: 'center' }}>
+                <Searchbar api_endpoint='api/instances/all' bar_width='75%' default_text='Find a test made on a valve instance' onChange_func={filterTests} />
+            </Grid>
+            <Grid item style={{ maxHeight: '75vh', overflowY: 'auto', paddingTop: '3vh' }}>
                 <Grid container spacing={2} justifyContent="center">
-                    {tests.map((d) => (
+                    {filteredTests.map((d) => (
                         <Grid item key={d._id}>
                             <Link to={`/tests/${d._id}`} style={{ textDecoration: 'none' }}>
                                 <CardTest instance_id={d.instance_id} timestamp={d.timestamp} arr_data={d.data} />
