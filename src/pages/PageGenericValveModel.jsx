4eb47691d91098878eba3449a21d7df88e88900c
marcoborelli <marco05.borelli@gmail.com>
2024-04-22 21:23:52 +0200

    \src\pages\PageGenericValveModel.jsx: aggiunta barra di ricerca alla pagina   riviste le proporzioni degli altri componenti

--- src/pages/PageGenericValveModel.jsx
+++ src/pages/PageGenericValveModel.jsx
@@ -4,10 +4,12 @@ import { Grid } from '@mui/material'
 import CardValveModel from '../components/CardValveModel'
 import Loading from '../components/Loading'
 import Navbar from '../components/Navbar'
+import Searchbar from '../components/Searchbar'
 import api from '../services/api'
 
 export default function PageGenericValveModel() {
     const [valves, setValves] = useState()
+    const [filteredValves, setFilteredValves] = useState()
     const [loading, setLoading] = useState(true)
 
     useEffect(() => {
@@ -15,6 +17,7 @@ export default function PageGenericValveModel() {
             try {
                 const response = await api.get(`api/valves/all`)
                 setValves(response.data)
+                setFilteredValves(response.data)
             } catch (error) {
                 console.error('Error fetching user data:', error)
             } finally {
@@ -25,6 +28,12 @@ export default function PageGenericValveModel() {
         fetchData()
     }, [])
 
+    function filterValveModels(e) {
+        setFilteredValves(valves.filter((valve) => {
+            return valve._id.toLowerCase().trim().includes(e.target.value.toLowerCase().trim())
+        }))
+    }
+
     if (loading) {
         return <Loading text="Caricamento in corso..."></Loading>
     }
@@ -32,9 +41,12 @@ export default function PageGenericValveModel() {
     return (
         <>
             <Navbar />
-            <Grid item style={{ paddingTop: '3vh' }}>
+            <Grid item style={{ paddingTop: '3vh', paddingBottom: '3vh', display: 'flex', justifyContent: 'center' }}>
+                <Searchbar api_endpoint='api/valves/all' bar_width='75%' default_text='Find a valve model' onChange_func={filterValveModels} />
+            </Grid>
+            <Grid item style={{ maxHeight: '75vh', overflowY: 'auto', paddingTop: '3vh' }}>
                 <Grid container spacing={2} justifyContent="center">
-                    {valves.map((valve) => (
+                    {filteredValves.map((valve) => (
                         <Grid item key={valve._id}>
                             <Link to={`/models/${valve._id}`} style={{ textDecoration: 'none' }}>
                                 <CardValveModel code={valve._id} description={valve.description} img_url={valve.img_url} />
