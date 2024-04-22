40e41ca90eada4d46ca3be2a9a96b478ff35222c
marcoborelli <marco05.borelli@gmail.com>
2024-04-22 21:22:30 +0200

    \src\components\Searchbar.jsx: aggiunto componente barra di ricerca di testi

---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react'
+import { Autocomplete, TextField } from '@mui/material'
+import api from '../services/api'
+
+export default function Searchbar({ api_endpoint, bar_width, default_text, onChange_func }) {
+    const [myOptions, setMyOptions] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await api.get(api_endpoint)
+                setMyOptions(response.data.map(val => val._id))
+
+                console.log(response.data)
+            } catch (error) {
+                console.error('Error fetching user data:', error)
+            }
+        }
+
+        fetchData()
+    }, [])
+
+
+    return (
+        <>
+            <Autocomplete
+                style={{ width: bar_width }}
+                freeSolo
+                autoComplete
+                autoHighlight
+                options={myOptions}
+                renderInput={(params) => (
+                    <TextField {...params}
+                        onChange={onChange_func}
+                        variant="outlined"
+                        label={default_text}
+                    />
+                )}
+            />
+        </>
+    )
+}
