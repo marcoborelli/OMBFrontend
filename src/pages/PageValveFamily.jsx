6194d2cf83b723b7f63cd243a29b0db362b5eb35
marcoborelli <marco05.borelli@gmail.com>
2024-04-26 22:46:53 +0200

    \src\pages\PageValveFamily.jsx: corretto bug casella di ricerca errata

--- src/pages/PageValveFamily.jsx
+++ src/pages/PageValveFamily.jsx
@@ -99,7 +99,7 @@ export default function PageValveModel() {
                 </Grid>
                 <Grid item xs={12} sm={9} style={{ paddingTop: '3vh' }}>
                     <Grid item style={{ paddingTop: '3vh', paddingBottom: '3vh', display: 'flex', justifyContent: 'center' }}>
-                        <Searchbar api_endpoint='api/families/all' bar_width='75%' default_text='Find a valve family' onChange_func={filterModels} />
+                        <Searchbar api_endpoint='api/valves/all' bar_width='75%' default_text='Find a valve model' onChange_func={filterModels} />
                     </Grid>
                     <Grid container spacing={2} justifyContent="center" style={{ maxHeight: '70vh', overflowY: 'auto', paddingTop: '3vh', }}>
                         {filteredModels.map((model) => (
