c39033491381608e421f24dcc87fb9a2478f01ce
marcoborelli <marco05.borelli@gmail.com>
2024-04-26 22:23:49 +0200

    \src\pages\PageGenericValveFamily.jsx: aggiunta pagina in cui sono presenti tutte le famiglie di valvole

---
+++ src/pages/PageGenericValveFamily.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Grid } from '@mui/material'
+import CardValveFamily from '../components/CardValveFamily'
+import Loading from '../components/Loading'
+import Navbar from '../components/Navbar'
+import Searchbar from '../components/Searchbar'
+import api from '../services/api'
+
+export default function PageGenericValveFamilies() {
+    const [families, setFamilies] = useState()
+    const [filteredFamilies, setFilteredFamilies] = useState()
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await api.get(`api/families/all`)
+                setFamilies(response.data)
+                setFilteredFamilies(response.data)
+            } catch (error) {
+                console.error('Error fetching user data:', error)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchData()
+    }, [])
+
+    function filterValveFamilies(e) {
+        setFilteredFamilies(families.filter((family) => {
+            return family._id.toLowerCase().trim().includes(e.target.value.toLowerCase().trim())
+        }))
+    }
+
+    if (loading) {
+        return <Loading text="Caricamento in corso..."></Loading>
+    }
+
+    return (
+        <>
+            <Navbar />
+            <Grid item style={{ paddingTop: '3vh', paddingBottom: '3vh', display: 'flex', justifyContent: 'center' }}>
+                <Searchbar api_endpoint='api/families/all' bar_width='75%' default_text='Find a valve family' onChange_func={filterValveFamilies} />
+            </Grid>
+            <Grid item style={{ maxHeight: '75vh', overflowY: 'auto', paddingTop: '3vh' }}>
+                <Grid container spacing={2} justifyContent="center">
+                    {filteredFamilies.map((family) => (
+                        <Grid item key={family._id}>
+                            <Link to={`/families/${family._id}`} style={{ textDecoration: 'none' }}>
+                                <CardValveFamily base_code={family._id} img_url={family.img_url} theoric_values={family.theoric_values} real_values={family.average_values} />
+                            </Link>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Grid>
+        </>
+    )
+}
