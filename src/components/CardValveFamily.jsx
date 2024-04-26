ee796f21062d0e30508c2e6e5ac851d4cc9a3f71
marcoborelli <marco05.borelli@gmail.com>
2024-04-26 22:22:50 +0200

    \src\components\CardValveFamily.jsx: aggiunto componente Card che rappresenta brevemente una famiglia di valvole

---
+++ src/components/CardValveFamily.jsx
@@ -0,0 +1,29 @@
+import * as React from 'react'
+import { CardActionArea, Typography, CardMedia, CardContent, Card } from '@mui/material'
+import TableMainAngles from './TableMainAngles'
+
+export default function CardValveFamily({ base_code, img_url, theoric_values, real_values }) {
+    return (
+        <Card sx={{ width: 330 }} style={{ backgroundColor: '#A2C6D8' }}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    image={img_url}
+                    alt={base_code}
+                    sx={{
+                        maxWidth: '100%',
+                        height: '180px',
+                        objectFit: 'none'
+                    }}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" align="center" component="div">
+                        {base_code}
+                    </Typography>
+
+                    <TableMainAngles theoric_values={theoric_values} real_values={real_values}/>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    )
+}
