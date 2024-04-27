a90eb3c3984551807894b8700819e500a236fd48
marcoborelli <marco05.borelli@gmail.com>
2024-04-28 00:32:13 +0200

    \src\pages\PageGenericValveFamily.jsx: riportata pagina all'aspetto originale.   Rimossa tabella di esempio e annessa legenda

--- src/pages/PageGenericValveFamily.jsx
+++ src/pages/PageGenericValveFamily.jsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { Grid, Divider, Chip, List, ListItem, ListItemAvatar, Avatar, ListItemText, Box } from '@mui/material'
+import { Grid } from '@mui/material'
 import CardValveFamily from '../components/CardValveFamily'
-import TableMainAngles from '../components/TableMainAngles'
 import Loading from '../components/Loading'
 import Navbar from '../components/Navbar'
 import Searchbar from '../components/Searchbar'
@@ -39,61 +38,21 @@ export default function PageGenericValveFamilies() {
         return <Loading text="Caricamento in corso..."></Loading>
     }
 
-    const fake_theoric_values = { bto: 106, runo: 40, eto: 78, btc: 106, runc: 40, etc: 65 }
-    const fake_actual_values = { bto: 108, runo: null, eto: 78, btc: 150, runc: 41, etc: 40 }
-
     return (
         <>
             <Navbar />
-            <Grid container>
-                <Grid item xs={12} sm={3} style={{ maxHeight: '75vh', overflowY: 'auto' }}>
-                    <Grid item style={{ paddingTop: '3vh', paddingBottom: '3vh', display: 'flex', justifyContent: 'center' }}>
-                        <Searchbar api_endpoint='api/families/all' bar_width='90%' default_text='Find a valve family' onChange_func={filterValveFamilies} />
-                    </Grid>
-
-                    <Divider>
-                        <Chip label="Table example & legend" size="small" />
-                    </Divider>
-
-                    <br />
-                    <Box component="section" sx={{ p: 2 }}>
-                        <TableMainAngles theoric_values={fake_theoric_values} real_values={fake_actual_values} colWidth={"20px"} showRowName />
-                    </Box>
-                    <br />
-
-                    <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
-                        <ListItem>
-                            <ListItemAvatar>
-                                <Avatar sx={{ bgcolor: '#FF7F7F', color: '#FF7F7F'}} variant='rounded'/>
-                            </ListItemAvatar>
-                            <ListItemText primary="Error" secondary="The average is higher than the theoric one (more than 30%)" />
-                        </ListItem>
-                        <ListItem>
-                            <ListItemAvatar>
-                                <Avatar sx={{ bgcolor: '#FCD299', color: '#FCD299' }} variant='rounded'/>
-                            </ListItemAvatar>
-                            <ListItemText primary="Warning" secondary="The average is higher than the theoric one (but less than 30%)" />
-                        </ListItem>
-                        <ListItem>
-                            <ListItemAvatar>
-                                <Avatar sx={{ bgcolor: '#EBDFE1', color: '#EBDFE1' }} variant='rounded'/>
-                            </ListItemAvatar>
-                            <ListItemText primary="Not Available" secondary="The average is not available for that angle" />
-                        </ListItem>
-                    </List>
-
-                    <Divider />
-                </Grid>
-                <Grid item xs={12} sm={9} style={{ maxHeight: '75vh', overflowY: 'auto', paddingTop: '3vh' }}>
-                    <Grid container spacing={2} justifyContent="center">
-                        {filteredFamilies.map((family) => (
-                            <Grid item key={family._id}>
-                                <Link to={`/families/${family._id}`} style={{ textDecoration: 'none' }}>
-                                    <CardValveFamily base_code={family._id} img_url={family.img_url} theoric_values={family.theoric_values} real_values={family.average_values} />
-                                </Link>
-                            </Grid>
-                        ))}
-                    </Grid>
+            <Grid item style={{ paddingTop: '3vh', paddingBottom: '3vh', display: 'flex', justifyContent: 'center' }}>
+                <Searchbar api_endpoint='api/families/all' bar_width='75%' default_text='Find a valve family' onChange_func={filterValveFamilies} />
+            </Grid>
+            <Grid item style={{ maxHeight: '75vh', overflowY: 'auto', paddingTop: '3vh' }}>
+                <Grid container spacing={2} justifyContent="center">
+                    {filteredFamilies.map((family) => (
+                        <Grid item key={family._id}>
+                            <Link to={`/families/${family._id}`} style={{ textDecoration: 'none' }}>
+                                <CardValveFamily base_code={family._id} img_url={family.img_url} theoric_values={family.theoric_values} real_values={family.average_values} />
+                            </Link>
+                        </Grid>
+                    ))}
                 </Grid>
             </Grid >
         </>
