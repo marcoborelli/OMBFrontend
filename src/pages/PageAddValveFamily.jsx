435ad6d83bc7abf860a90d51b340bb451ab19066
marcoborelli <marco05.borelli@gmail.com>
2024-04-29 21:32:22 +0200

    \src\pages\PageAddValveFamily.jsx: creata pagina per l'aggiunta della famiglia della valvole

---
+++ src/pages/PageAddValveFamily.jsx
@@ -0,0 +1,29 @@
+import { Container, Grid, Paper } from '@mui/material'
+import Navbar from '../components/Navbar'
+import AddValveFamily from '../components/AddValveFamily'
+import api from '../services/api'
+
+export default function PageGenericTest() {
+    const addNewValve = async (to_insert) => {
+        try {
+            await api.post('api/families/add', to_insert);
+        } catch (error) {
+            console.error('Error adding new family', error);
+        }
+
+        window.location.href = '/families'
+    }
+
+    return (
+        <>
+            <Navbar />
+            <Container maxWidth="md" style={{ marginTop: '6vh', marginBottom: '20vh' }}>
+                <Grid container justifyContent="center" alignItems="center" style={{ height: '100%' }}>
+                    <Paper elevation={3} style={{ padding: 20, borderRadius: 10, backgroundColor: 'white', width: '100%' }}>
+                        <AddValveFamily onSubmit_func={addNewValve} />
+                    </Paper>
+                </Grid>
+            </Container>
+        </>
+    )
+}
