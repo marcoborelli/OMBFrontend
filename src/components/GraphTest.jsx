c7a2d25776a08bd555783755e5a075fc42782593
marcoborelli <marco05.borelli@gmail.com>
2024-05-02 22:26:15 +0200

    \src\components\GraphTest.jsx: risolto bug mancata visualizzazione menu con dati aggiuntivi allo scorrimento del mouse sul grafico

--- src/components/GraphTest.jsx
+++ src/components/GraphTest.jsx
@@ -5,6 +5,15 @@ export default function GraphTest({ height, data, isInCard, isClosing }) {
     let axisData = data.map(val => val.angle)
     let pairData = data.map(val => val.pair)
 
+    if (isClosing) {
+        //non so il motivo ma serve che gli angoli siano crescenti senno' non compare la tebella con il valore associato allo scorrimento del mouse
+        axisData.sort(function (a, b) {
+            return a - b;
+        })
+        //di conseguenza inverto ance l'array della coppia
+        pairData = pairData.reverse()
+    }
+
     return (
         <LineChart
             xAxis={[{
