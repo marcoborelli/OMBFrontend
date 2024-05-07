2935cba45ee94cd6486e0ced24395e0231fb0021
marcoborelli <marco05.borelli@gmail.com>
2024-05-07 18:09:07 +0200

    \src\pages\PageValveModel.jsx: aggiunto collegamento per la creazione di una nuova istanza di un modello

--- src/pages/PageValveModel.jsx
+++ src/pages/PageValveModel.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
-import { Card, CardContent, CardMedia, TextField, Grid } from '@mui/material'
+import { Card, CardContent, CardMedia, TextField, Grid, IconButton } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add'
 import CardValveInstance from '../components/CardValveInstance'
 import TableMainAngles from '../components/TableMainAngles'
 import Legend from '../components/Legend'
@@ -145,6 +146,9 @@ export default function PageValveModel() {
                 <Grid item xs={12} sm={9} style={{ paddingTop: '3vh' }}>
                     <Grid item style={{ paddingTop: '3vh', paddingBottom: '3vh', display: 'flex', justifyContent: 'center' }}>
                         <Searchbar api_endpoint='api/instances/all' bar_width='75%' default_text='Find a valve model instance' onChange_func={filterInstances} />
+                        <IconButton aria-label="add instance" size="large" href={`/instances/add/${valve._id}`}>
+                            <AddIcon />
+                        </IconButton>
                     </Grid>
                     <Grid container spacing={2} justifyContent="center" style={{ maxHeight: '70vh', overflowY: 'auto', paddingTop: '3vh', }}>
                         {filteredInstances.map((instance) => (
