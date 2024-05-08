02ce9b424df7dfe613a3a2c211f3e5ec3aa3f8dd
marcoborelli <marco05.borelli@gmail.com>
2024-05-08 17:26:05 +0200

    \\src\main.jsx: rimossa modalità sviluppo (inizializzava due volte anzichè una i componenti)

--- src/main.jsx
+++ src/main.jsx
@@ -66,7 +66,5 @@ const router = createBrowserRouter([
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
