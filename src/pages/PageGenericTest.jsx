9c9c21d8619c06e3efc75dfcbc5a531420b5d4a8
marcoborelli <marco05.borelli@gmail.com>
2024-05-11 21:10:30 +0200

    \src\pages\PageGenericTest.jsx: modificata funzione di ricerca che adesso supporta l'impaginazione e avviene non più sui risultati visibili ma direttamente con un'api specifica al backend   Eliminata funzione getTestInfo. Questi dati vengono infatti adesso inviati ad ogni richiesta e non più una volta sola

--- src/pages/PageGenericTest.jsx
+++ src/pages/PageGenericTest.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { Grid, Box, CircularProgress } from '@mui/material'
+import { Grid, Box, CircularProgress, Button } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search'
 import CardTest from '../components/CardTest'
 import Navbar from '../components/Navbar'
 import Searchbar from '../components/Searchbar'
@@ -8,22 +9,30 @@ import api from '../services/api'
 
 export default function PageGenericTest() {
     const [tests, setTests] = useState([])
-    const [filteredTests, setFilteredTests] = useState([])
-    const [currentPage, setCurrentPage] = useState(1)
+    const [currentPage, setCurrentPage] = useState(0)
     const [pagesNumber, setPagesNumber] = useState()
     const [isLoading, setIsLoading] = useState(true)
 
+    const [filterInstanceId, setFilterInstanceId] = useState('')
+    const [isNewSearch, setIsNewSearch] = useState(true)
+
     const fetchData = async () => {
-        if (currentPage > pagesNumber)
+        if (!isNewSearch && currentPage >= pagesNumber)
             return
 
         setIsLoading(true)
         try {
-            const response = await api.get(`api/tests/all?page_number=${currentPage}`)
+            const response = await api.get(`api/tests/find?instance_id=${filterInstanceId}&page_number=${isNewSearch ? 1 : currentPage + 1}`)
 
-            setTests([...tests, ...response.data])
-            setFilteredTests([...filteredTests, ...response.data])
-            setCurrentPage(currentPage + 1)
+            if (isNewSearch) {
+                setIsNewSearch(false)
+                setTests(response.data.query_data)
+                setPagesNumber(Math.ceil(parseInt(response.data.elements_number) / parseInt(response.data.elements_for_page)))
+                setCurrentPage(1)
+            } else {
+                setTests([...tests, ...response.data.query_data])
+                setCurrentPage(currentPage + 1)
+            }
         } catch (error) {
             console.error('Error fetching user data:', error)
         } finally {
@@ -31,19 +40,12 @@ export default function PageGenericTest() {
         }
     }
 
-    const getTestInfo = async () => {
-        try {
-            const response = await api.get(`api/tests/info`)
-            setPagesNumber(Math.ceil(parseInt(response.data.elements_number) / parseInt(response.data.elements_for_page)))
-        } catch (error) {
-            console.error('Error fetching infos:', error)
-        }
-    }
-
     useEffect(() => {
+        if (!isNewSearch)
+            return
+
         fetchData()
-        getTestInfo()
-    }, [])
+    }, [isNewSearch])
 
     const handleScroll = (e) => {
         if (!isLoading && e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight) {
@@ -51,21 +53,18 @@ export default function PageGenericTest() {
         }
     }
 
-    function filterTests(e) {
-        setFilteredTests(tests.filter((test) => {
-            return test.instance_id.toLowerCase().trim().includes(e.target.value.toLowerCase().trim())
-        }))
-    }
-
     return (
         <>
             <Navbar />
             <Grid item style={{ paddingTop: '3vh', paddingBottom: '3vh', display: 'flex', justifyContent: 'center' }}>
-                <Searchbar api_endpoint='api/instances/all' bar_width='75%' default_text='Find a test made on a valve instance' onChange_func={filterTests} />
+                <Searchbar api_endpoint='api/instances/all' bar_width='75%' default_text='Find a test made on a valve instance' onChange_func={(e) => setFilterInstanceId(e.target.value.toUpperCase())} />
+                <Button variant="outlined" onClick={() => setIsNewSearch(true)}>
+                    <SearchIcon />
+                </Button>
             </Grid>
             <Grid item style={{ maxHeight: '75vh', overflowY: 'auto', paddingTop: '3vh' }} onScroll={handleScroll}>
                 <Grid container spacing={2} justifyContent="center">
-                    {filteredTests.map((d) => (
+                    {tests.map((d) => (
                         <Grid item key={d._id}>
                             <Link to={`/tests/${d._id}`} style={{ textDecoration: 'none' }}>
                                 <CardTest instance_id={d.instance_id} timestamp={d.timestamp} arr_data={d.data} />
@@ -73,7 +72,7 @@ export default function PageGenericTest() {
                         </Grid>
                     ))}
                 </Grid>
-                {(currentPage <= pagesNumber || !pagesNumber) &&
+                {(currentPage < pagesNumber || !pagesNumber) &&
                     <Box flexDirection="column" justifyContent="center" alignItems="center" sx={{ display: 'flex', paddingTop: '3vh' }}>
                         <CircularProgress />
                     </Box>
